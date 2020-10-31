4161d9b8b1e564002b17153d24e5e681ad661718
Venca Lysek <vencalysek@seznam.cz>
2020-10-31 21:56:02 +0100

    added button in movie details

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -1,13 +1,12 @@
 import React from "react";
-import heroImg from '../img/movie_hero.jpg'
-import posterImg from '../img/poster.jpg'
+import heroImg from "../img/movie_hero.jpg";
+import posterImg from "../img/poster.jpg";
 
 const MovieDetails = () => {
-
     // image styling
     const styleHeroImage = {
         backgroundImage: `url(${heroImg})`,
-    }
+    };
 
     // colorst for imge overlay
     const colors = [
@@ -25,19 +24,24 @@ const MovieDetails = () => {
     const colorRand = Math.floor(Math.random() * colors.length);
 
     const styleHeroOverlay = {
-        backgroundImage: `linear-gradient(to right, ${colors[colorRand][0]} 150px,${colors[colorRand][1]} 110%`
+        backgroundImage: `linear-gradient(to right, ${colors[colorRand][0]} 150px,${colors[colorRand][1]} 110%`,
     };
-
 
     return (
         <div className="movie-details">
             <div className="movie-details__hero">
-                <div className="movie-details__hero--img" style={styleHeroImage} >
-                    <div className="movie-details__hero--overlay" style={styleHeroOverlay}>
+                <div
+                    className="movie-details__hero--img"
+                    style={styleHeroImage}
+                >
+                    <div
+                        className="movie-details__hero--overlay"
+                        style={styleHeroOverlay}
+                    >
                         <div className="movie-details--wrap">
                             {/* poster */}
                             <div className="movie-details__poster--wrap">
-                                <img src={posterImg} alt=""/>
+                                <img src={posterImg} alt="" />
                             </div>
                             {/* info */}
                             <div className="movie-details__info--wrap">
@@ -48,8 +52,12 @@ const MovieDetails = () => {
                                             Warcraft <span>(2016)</span>
                                         </h1>
                                         <div className="info--content__heading-facts">
-                                            <span className="certification">R</span>
-                                            <span className="release">10/10/2020</span>
+                                            <span className="certification">
+                                                R
+                                            </span>
+                                            <span className="release">
+                                                10/10/2020
+                                            </span>
 
                                             {/* TADY BUDE TREBA UDELAT MAP NA ZANR FILMU */}
                                             <span className="info--content__heading-facts-genres">
@@ -65,17 +73,35 @@ const MovieDetails = () => {
                                     {/* score */}
                                     {/* overview */}
                                     <div className="info--content__overview">
-                                        <h1 className="info--content__overview-title">Overview</h1>
+                                        <h1 className="info--content__overview-title">
+                                            Overview
+                                        </h1>
                                         <p className="info--content__overview-content">
-                                        The peaceful realm of Azeroth stands on the brink of war as its civilization faces a fearsome race of invaders: orc warriors fleeing their dying home to colonize another. As a portal opens to connect the two worlds, one army faces destruction and the other faces extinction. From opposing sides, two heroes are set on a collision course that will decide the fate of their family, their people, and their home.
+                                            The peaceful realm of Azeroth stands
+                                            on the brink of war as its
+                                            civilization faces a fearsome race
+                                            of invaders: orc warriors fleeing
+                                            their dying home to colonize
+                                            another. As a portal opens to
+                                            connect the two worlds, one army
+                                            faces destruction and the other
+                                            faces extinction. From opposing
+                                            sides, two heroes are set on a
+                                            collision course that will decide
+                                            the fate of their family, their
+                                            people, and their home.
                                         </p>
-
                                     </div>
                                 </section>
                             </div>
                         </div>
                     </div>
                 </div>
+            </div>
+            <div className="movie-details__back-link">
+                <a href="#">
+                    <i className="material-icons">arrow_back</i>
+                </a>
             </div>
         </div>
     );
