f35a4742d76df213716e2c397b6d9760618cac07
Venca Lysek <vencalysek@seznam.cz>
2020-11-02 16:40:15 +0100

    sidebar router active

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,43 +1,64 @@
 import React, { useState } from "react";
-import { Link } from 'react-router-dom'
-
+import { NavLink } from "react-router-dom";
 
 const Sidebar = () => {
-
     return (
         <div className="sidebar">
             <ul className="side-nav">
-                <li className="side-nav__item side-nav__item--active">
-                    <Link to="/" className="side-nav__link">
+                <li className="side-nav__item">
+                    <NavLink
+                        exact
+                        to="/"
+                        className="side-nav__link"
+                        activeClassName="side-nav__link--active"
+                    >
                         <i className="material-icons side-nav__icon">home</i>
-                        <span className="side-nav__text" >Home</span>
-                    </Link>
+                        <span className="side-nav__text">Home</span>
+                    </NavLink>
                 </li>
                 <li className="side-nav__item">
-                    <Link to="/now-playing" className="side-nav__link">
+                    <NavLink
+                        to="/now-playing"
+                        className="side-nav__link"
+                        activeClassName="side-nav__link--active"
+                    >
                         <i className="material-icons side-nav__icon">airplay</i>
-                        <span className="side-nav__text" >Now Playing</span>
-                    </Link>
+                        <span className="side-nav__text">Now Playing</span>
+                    </NavLink>
                 </li>
                 <li className="side-nav__item">
-                    <Link to="/popular" className="side-nav__link">
-                        <i className="material-icons side-nav__icon">whatshot</i>
-                        <span className="side-nav__text" >Popular</span>
-                    </Link>
+                    <NavLink
+                        to="/popular"
+                        className="side-nav__link"
+                        activeClassName="side-nav__link--active"
+                    >
+                        <i className="material-icons side-nav__icon">
+                            whatshot
+                        </i>
+                        <span className="side-nav__text">Popular</span>
+                    </NavLink>
                 </li>
                 <li className="side-nav__item">
-                    <Link to="/top-rated" className="side-nav__link">
+                    <NavLink
+                        to="/top-rated"
+                        className="side-nav__link"
+                        activeClassName="side-nav__link--active"
+                    >
                         <i className="material-icons side-nav__icon">star</i>
-                        <span className="side-nav__text" >Top Rated</span>
-                    </Link>
+                        <span className="side-nav__text">Top Rated</span>
+                    </NavLink>
                 </li>
                 <li className="side-nav__item">
-                    <Link to="/upcoming" className="side-nav__link">
+                    <NavLink
+                        to="/upcoming"
+                        className="side-nav__link"
+                        activeClassName="side-nav__link--active"
+                    >
                         <i className="material-icons side-nav__icon">
                             fast_forward
                         </i>
-                        <span className="side-nav__text" >Upcoming</span>
-                    </Link>
+                        <span className="side-nav__text">Upcoming</span>
+                    </NavLink>
                 </li>
             </ul>
         </div>
