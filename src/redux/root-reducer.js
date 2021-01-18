c09ca925477e9e477e1005ed370063b6290d90eb
Venca Lysek <vencalysek@seznam.cz>
2021-01-18 11:21:38 +0100

    seting up redux for comments

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -2,11 +2,13 @@ import {combineReducers} from "redux";
 import fetchMoviesReducer from './movies/movies.reducer'
 import fetchMovieDetailsReducer from './movie-details/movie-details.reducer'
 import fetchCastsReducer from './casts/casts.reducer'
+import fetchCommentsReducer from './comments/comments.reducer'
 
 const rootReducer = combineReducers({
   movies: fetchMoviesReducer,
   movieDetails: fetchMovieDetailsReducer,
-  casts: fetchCastsReducer
+  casts: fetchCastsReducer,
+  comments: fetchCommentsReducer
 });
 
 export default rootReducer;
